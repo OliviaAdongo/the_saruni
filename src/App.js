ee2ad7598f582d387de65225c782d8d88ca4a727
Olivia Adongo <akinyioliviatove@gmail.com>
2023-03-11 07:40:00 +0300

    Milestone: Routes and navbar created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AmenitiesHomePage from './Amenities/AmenitiesHomePage';
 import './App.css';
 import Home from './Home/Home';
+import Navbar from './Navbar/Navbar';
 import NeighborhoodHomepage from './Neighborhood/NeighborhoodHomepage';
 import ResidenceHomePage from './Residence/ResidenceHomePage';
 
 function App() {
   return (
-   <div>
-    <Home/>
-    <ResidenceHomePage/>
-    <AmenitiesHomePage/>
-    <NeighborhoodHomepage/>
-   </div>
+   <Router>
+   <Navbar/>
+    <Routes>
+      <Route path='/'  element={<Home/>}/>
+      <Route path='/residencehomepage'  element={<ResidenceHomePage/>}/>
+      <Route path='/amenitieshomepage'  element={<AmenitiesHomePage/>}/>
+      <Route path='/neighborhoodhomepage'  element={<NeighborhoodHomepage/>}/>
+      <Route path='/residencehomepage'  element={<ResidenceHomePage/>}/>
+
+    </Routes>
+   </Router>
   );
 }
 
