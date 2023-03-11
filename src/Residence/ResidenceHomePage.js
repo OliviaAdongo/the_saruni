82098b0b116c4601fce38a7299c4d5026a54dced
Olivia Adongo <akinyioliviatove@gmail.com>
2023-03-11 08:41:05 +0300

    Milestone: Routes created to child residence pages

--- src/Residence/ResidenceHomePage.js
+++ src/Residence/ResidenceHomePage.js
@@ -1,13 +1,16 @@
 import './Residence.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function ResidenceHomePage() {
+  let navigate = useNavigate();
   return (
     <div className="residence">
       <div className="left">
         <p>logo</p>
         <h4>RESIDE IN LUXURY</h4>
         <h5> surround lorem ipsum lorem ipsum</h5>
-        <button>THE RESIDENCES</button>
+        <button  onClick={()=>{ navigate('/residence')}}>THE RESIDENCES</button>
       </div>
       <div className="right">
         <p>There is  an image here</p>
--- src/Residence/Residences.js
+++ src/Residence/Residences.js
@@ -1,6 +1,8 @@
 import './Residence.css'
+import { useNavigate } from 'react-router-dom';
 
 function Residences() {
+  let navigate = useNavigate();
   return (
     <div>
       <div className='residence-left'>
@@ -8,7 +10,12 @@ function Residences() {
         <h2>LUXURY</h2>
         <p>Lorem ipsums lorem ipsums lorem ipsums</p>
       </div>
-      <div className='residence-right'></div>
+      <div className='residence-right'>
+        <p>here goes an image</p>
+      </div>
+      <div className='bottom'>
+        <button onClick={()=>{ navigate('/livingroom')}} >EXPLORE MORE</button>
+      </div>
     </div>
   )
 }
