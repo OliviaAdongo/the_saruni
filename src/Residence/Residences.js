3a81f5bb76a8c1003df82f2810a8abb5937fc9a0
Olivia Adongo <akinyioliviatove@gmail.com>
2023-03-11 08:57:20 +0300

    Milestone: Residence main page lorem ipsum changed to actual content

--- src/Residence/Residences.js
+++ src/Residence/Residences.js
@@ -1,23 +1,34 @@
-import './Residence.css'
-import { useNavigate } from 'react-router-dom';
+import "./Residence.css";
+import { useNavigate } from "react-router-dom";
 
 function Residences() {
   let navigate = useNavigate();
   return (
     <div>
-      <div className='residence-left'>
-        <p>LOGO CARD</p>
-        <h2>LUXURY</h2>
-        <p>Lorem ipsums lorem ipsums lorem ipsums</p>
+      <div className="residence-left">
+        <p>LOGO CARD the residences</p>
+        <h2>LUXURY Units</h2>
+        <h3>CAREFULLY CONSIDERED AND METICULOUSLY DESIGNED</h3>
+        <h4>
+          With uninterrupted views from each appartments, 'The Saruni' at
+          Riverside Drive, is a modern interpretation of contemporary luxury
+          specifically designed to enhance your living exparience
+        </h4>
       </div>
-      <div className='residence-right'>
+      <div className="residence-right">
         <p>here goes an image</p>
       </div>
-      <div className='bottom'>
-        <button onClick={()=>{ navigate('/livingroom')}} >EXPLORE MORE</button>
+      <div className="bottom">
+        <button
+          onClick={() => {
+            navigate("/livingroom");
+          }}
+        >
+          EXPLORE THE RESIDENCES
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Residences
+export default Residences;
